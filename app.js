6bfb5083a59718525667af75d2681df7ba04b964
dib6596 <cory.harkins@cerecore.net>
2020-09-20 18:00:55 -0500

    Remove route handlers

--- app.js
+++ app.js
@@ -3,29 +3,11 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse
 const express =require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-  // res.send('<h1>Hey!</h1>')
-  const twiml = new VoiceResponse();
-
-  twiml.say('Hello from your pals at Twilio! Have fun.');
-
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
-  res.end(twiml.toString());
-})
-
-app.get('/twiml', (req, res) => {
-  const twiml = new VoiceResponse();
-
-  twiml.say('Hello from Cory Harkins, testing a node.js application he just built!')
-  res.writeHead(200, { 'Content-Type': 'text/xml'})
-  res.end(twiml.toString())
-})
-
 http.createServer((req, res)=>{
   console.log('The app is running');
   const twiml = new VoiceResponse();
 
-  twiml.say('Hello from your pals at Twilio! Have fun.');
+  twiml.say('Hello from Cory Harkins, testing a node.js application he just built!');
 
   res.writeHead(200, { 'Content-Type': 'text/xml' });
   res.end(twiml.toString());
