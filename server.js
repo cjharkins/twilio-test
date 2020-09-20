58e92b8c6e9dae86e77c8003a4e53a03716f424a
dib6596 <cory.harkins@cerecore.net>
2020-09-20 15:06:06 -0500

    Add express routes for ui

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const http = require('http')
 const VoiceResponse = require('twilio').twiml.VoiceResponse
+const express =require('express');
+const app = express();
+const PORT = 1337;
 
-http.createServer((req, res) => {
+
+app.get('/',(req,res)=>{
+  res.json({message: 'Welcome to the backend'})
+})
+
+app.get('/twiml', (req, res) => {
   const twiml = new VoiceResponse();
 
   twiml.say('Hello from Cory Harkins, testing a node.js application he just built!')
   res.writeHead(200, { 'Content-Type': 'text/xml'})
   res.end(twiml.toString())
-}).listen(1337, '127.0.0.1');
+})
 
-console.log('TwiML server running at http://127.0.0.1:1337/');
+app.listen(PORT,(req, res) => console.log('Listening on port 1337'))
